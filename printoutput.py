0131a4481e8b9c9c00b76d03062565e87d29d5fe
christopher <christopher.kullenberg@gmail.com>
2015-12-26 19:45:50 +0100

    made print script sort alphabetically

--- printoutput.py
+++ printoutput.py
@@ -3,7 +3,12 @@ import csv
 #Open and parse the output file
 outputfile = open('output.csv','r') # change file-name here
 outputdata = csv.reader(outputfile, delimiter=',', quotechar='"')
+next(outputdata) #remove header by skipping first line
 
-#Print a pretty bibliography
-for o in outputdata:
+
+
+
+
+#Print a pretty bibliography, alphabetically .
+for o in sorted(outputdata):
     print(o[0] + ", (" + o[1] + ') "' + o[2] + '", ' + o[3] + ", (" + o[4] + ": " + o[5] + ")\n")
